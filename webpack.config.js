e37de995c081704a4b98fc0a41afa3cc5fc90668
Mikael <dev.kov.mtl@gmail.com>
2021-10-20 11:03:29 -0400

    Added babel.config and babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,18 @@ module.exports = {
     assetModuleFilename: 'images/[hash][ext][query]',
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 }
