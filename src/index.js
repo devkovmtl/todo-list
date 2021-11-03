5f92223f9493294afdce648307f28e540a338bc0
Mikael <dev.kov.mtl@gmail.com>
2021-11-03 16:58:58 -0400

    Added Project Modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import './styles/index.css'
 
-const body = document.querySelector('body#body')
+const btnMenuNav = document.querySelector('.btnMenuNav')
+const sideNav = document.querySelector('.side-nav')
+const overlay = document.querySelector('.overlay')
+const addProjectBtn = document.querySelector('.add-project-btn')
+const projectModal = document.querySelector('.project-modal')
+const submitBtnProject = document.querySelector('.submit-btn-project')
+
+btnMenuNav.addEventListener('click', (e) => {
+  sideNav.classList.toggle('-translate-x-full')
+  overlay.classList.toggle('hidden')
+})
+
+overlay.addEventListener('click', (e) => {
+  sideNav.classList.toggle('-translate-x-full')
+  overlay.classList.toggle('hidden')
+})
+
+addProjectBtn.addEventListener('click', (e) => {
+  projectModal.classList.remove('hidden')
+})
+
+submitBtnProject.addEventListener('click', (e) => {
+  projectModal.classList.add('hidden')
+})
 // body.innerHTML += `
 // <header class="flex items-center justify-between">
 // <h2 class="text-lg leading-6 font-medium bg-gray-900 text-green-500">Projects test</h2>
