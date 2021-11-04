4bf959c225ea46d5274f88f8f53ae8b4d9ea4665
Mikael <dev.kov.mtl@gmail.com>
2021-11-04 14:25:01 -0400

    Project Modal and Todo Modal Added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import './styles/index.css'
 const btnMenuNav = document.querySelector('.btnMenuNav')
 const sideNav = document.querySelector('.side-nav')
 const overlay = document.querySelector('.overlay')
+
 const addProjectBtn = document.querySelector('.add-project-btn')
 const projectModal = document.querySelector('.project-modal')
 const submitBtnProject = document.querySelector('.submit-btn-project')
+const cancelBtnProject = document.querySelector('.cancel-btn-project')
+
+const addBtnTodo = document.querySelector('.add-btn-todo')
+const todoModal = document.querySelector('.todo-modal')
+const submitBtnTodo = document.querySelector('.submit-btn-todo')
+const cancelBtnTodo = document.querySelector('.cancel-btn-todo')
 
 btnMenuNav.addEventListener('click', (e) => {
   sideNav.classList.toggle('-translate-x-full')
@@ -18,12 +25,28 @@ overlay.addEventListener('click', (e) => {
 })
 
 addProjectBtn.addEventListener('click', (e) => {
-  projectModal.classList.remove('hidden')
+  projectModal.classList.toggle('hidden')
+})
+
+addBtnTodo.addEventListener('click', (e) => {
+  todoModal.classList.toggle('hidden')
+})
+
+cancelBtnProject.addEventListener('click', (e) => {
+  projectModal.classList.add('hidden')
+})
+cancelBtnTodo.addEventListener('click', (e) => {
+  todoModal.classList.add('hidden')
 })
 
 submitBtnProject.addEventListener('click', (e) => {
   projectModal.classList.add('hidden')
 })
+
+submitBtnTodo.addEventListener('click', (e) => {
+  todoModal.classList.add('hidden')
+})
+
 // body.innerHTML += `
 // <header class="flex items-center justify-between">
 // <h2 class="text-lg leading-6 font-medium bg-gray-900 text-green-500">Projects test</h2>
